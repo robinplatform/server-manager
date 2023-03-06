ce10ac14bbd7b9e4f5a848f9e09dd65c87f824e4
Karim Alibhai <karim@alibhai.co>
2023-03-06 01:29:28 -0500

    more setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/robinplatform/server-manager
+
+go 1.20
--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,28 @@
-import React from "react";
-import { renderApp } from "@robinplatform/toolkit/react";
-import "@robinplatform/toolkit/styles.css";
-import "./global.scss";
 import "./app.scss";
+import "./global.scss";
+import { renderApp } from "@robinplatform/toolkit/react";
+import { useRemoteAppMethod } from "@robinplatform/toolkit/react/rpc";
+import "@robinplatform/toolkit/styles.css";
+import React from "react";
+import { z } from "zod";
 
 const App = () => {
-  return (
-    <div className="appContainer robin-pad robin-bg-dark-blue robin-rounded">
-      <p>hello world</p>
-    </div>
-  );
+	const { data, error } = useRemoteAppMethod(
+		"getHelloWorld",
+		{},
+		{
+			resultType: z.object({ message: z.string() }),
+		},
+	);
+
+	return (
+		<div className="appContainer robin-pad robin-bg-dark-blue robin-rounded">
+			<>
+				{error && <p>{String(error)}</p>}
+				{data && <p>{data.message}</p>}
+			</>
+		</div>
+	);
 };
 
 renderApp(<App />);
