2c907f45be9cfd7794b391bdd60cdf02f9e19a9b
Albert Liu <albertymliu@gmail.com>
2023-03-13 15:18:01 -0700

    move button to control panel

--- src/ControlPanel/index.tsx
+++ src/ControlPanel/index.tsx
@@ -1,16 +1,34 @@
 import { useHistory } from "../hooks/useHistory";
 import { useSelectedServer } from "../hooks/useSelectedServer";
+import { runAppMethod } from "@robinplatform/toolkit";
+import { useMutation } from "@tanstack/react-query";
 import React from "react";
+import { z } from "zod";
 
 export const ControlPanel: React.FC = () => {
 	const { selectedServer } = useSelectedServer();
 	const history = useHistory();
+	const { mutate } = useMutation<string, unknown, unknown, [string]>({
+		mutationKey: ["StartServer"],
+		mutationFn: () => {
+			runAppMethod({
+				methodName: "StartServer",
+				resultType: z.any(),
+				data: {
+					server: selectedServer,
+				},
+			});
+			return null;
+		},
+	});
 
 	return (
 		<div className='serverControlPanel'>
 			<h1 className='serverControlPanelHeading'>{selectedServer}</h1>
 
 			<p>{history.pathname}</p>
+
+			<button onClick={mutate}>Start</button>
 		</div>
 	);
 };
--- src/app.tsx
+++ src/app.tsx
@@ -1,37 +1,18 @@
 import { ControlPanel } from "./ControlPanel";
 import { ServerList } from "./ServerList";
 import "./app.scss";
-import { useRpcQuery } from "./bridge";
 import "./global.scss";
 import { useSelectedServer } from "./hooks/useSelectedServer";
-import { runAppMethod } from "@robinplatform/toolkit";
 import { renderApp } from "@robinplatform/toolkit/react";
 import "@robinplatform/toolkit/styles.css";
-import { useMutation } from "@tanstack/react-query";
 import React from "react";
-import { z } from "zod";
 
 const App = () => {
 	const { selectedServer } = useSelectedServer();
-	const { mutate } = useMutation<string, unknown, unknown, [string]>({
-		mutationKey: ["StartServer"],
-		mutationFn: () => {
-			runAppMethod({
-				methodName: "StartServer",
-				resultType: z.any(),
-				data: {
-					server: selectedServer,
-				},
-			});
-			return null;
-		},
-	});
 
 	return (
 		<div className="appContainer robin-pad robin-bg-dark-blue robin-rounded">
 			<div className="leftSidebar">
-				<button onClick={mutate}>Start Server</button>
-
 				<ServerList />
 			</div>
 
